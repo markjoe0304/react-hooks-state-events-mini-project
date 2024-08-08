bdc314b0ca88457de8b36d7b09bef567a59caa2b
markjoe0304 <markjoegicheru@gmail.com>
2024-08-08 14:54:26 +0300

    test completed

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from 'react';
 
-function CategoryFilter() {
+function CategoryFilter({ categories, onSelect }) {
+  const [selectedCategory, setSelectedCategory] = useState('All');
+
+  const handleClick = (category) => {
+    setSelectedCategory(category);
+    onSelect(category);
+  };
+
   return (
-    <div className="categories">
-      <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+    <div>
+      {categories.map(category => (
+        <button
+          key={category}
+          className={selectedCategory === category ? 'selected' : ''}
+          onClick={() => handleClick(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
